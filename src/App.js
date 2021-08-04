72101e4f46e79eae309c880941b6f2febd518aae
Malek-Ghorbel <malekghorbel@insat.u-carthage.tn>
2021-08-04 16:11:44 +0100

    adding components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from "react-tsparticles";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+      options={{
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 4,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.4,
+          },  
+        },
+      }}/>
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react" ;
+
+const Navigation = () => {
+    return (
+        <nav style={{display: "flex" , justifyContent:'flex-end'}} >
+            <span className='f3 link dim black underline pa3 pointer'>Sign out</span>
+        </nav>
+    )
+}
+
+export default Navigation ;
